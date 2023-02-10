18ac33865bd18e8c33819a6d19d02389515f9895
paulolima1212 <plima12121984@gmail.com>
2023-02-10 14:44:53 +0000

    sure: change port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
+  const port = process.env.PORT;
 
   app.setGlobalPrefix('/_api/v2.1/bolacha');
   app.useGlobalPipes(
@@ -16,20 +17,19 @@ async function bootstrap() {
     }),
   );
 
-  const version = 'v2.1'
-  // swagger config
+  const version = 'v2.1';
 
   const config = new DocumentBuilder()
-  .setTitle('Bolacha maria api')
-  .setDescription(`Bolacha maria api ${version} is open`)
-  .setVersion(version)
-  .addTag('Bolacha')
-  .build()
+    .setTitle('Bolacha maria api')
+    .setDescription(`Bolacha maria api ${version} is open`)
+    .setVersion(version)
+    .addTag('Bolacha')
+    .build();
 
-  const document =  SwaggerModule.createDocument(app,config)
+  const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api',app,document)
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
