d6d3f7facb65c2acb5dc457b82b6e5deecec8464
paulolima1212 <plima12121984@gmail.com>
2023-02-24 14:09:42 +0000

    feat: change route stock products

--- src/stock_product/stock_product.controller.ts
+++ src/stock_product/stock_product.controller.ts
@@ -10,15 +10,12 @@ import {
 import { StockProductService } from './stock_product.service';
 import { CreateStockProductDto } from './dto/create-stock_product.dto';
 import { UpdateStockProductDto } from './dto/update-stock_product.dto';
-import { UseGuards } from '@nestjs/common/decorators';
-import { AuthGuard } from '@nestjs/passport';
 import { ParseUUIDPipe } from '@nestjs/common/pipes';
 
 @Controller('stock-products')
 export class StockProductController {
   constructor(private readonly stockProductService: StockProductService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createStockProductDto: CreateStockProductDto) {
     return this.stockProductService.create(createStockProductDto);
@@ -29,7 +26,6 @@ export class StockProductController {
     return this.stockProductService.findOne(id);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -38,7 +34,6 @@ export class StockProductController {
     return this.stockProductService.update(id, updateStockProductDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.stockProductService.remove(id);
